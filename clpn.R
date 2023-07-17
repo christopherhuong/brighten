c1a9eba24dd6723cfbd055ff7511b2143d787b1d
huongtopher <chrishuong95@gmail.com>
2023-07-17 04:05:19 -0500

    asd

--- clpn.R
+++ clpn.R
@@ -1,7 +1,22 @@
 
 library(tidyverse)
-library(rio)
 
+
+dat <- read.csv('phq9.csv')
+
+
+dat1 <- drop_na(dat)
+
+
+glimpse(dat)
+
+dat[1,3]
+dat[1,3] <- "BLUE-00050"
+dat[1,3]
+
+dat$participant_id <- as.factor(dat$participant_id)
+
+summary(dat$participant_id)
 
 
 
