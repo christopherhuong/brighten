4051a0ee6837b2fafb820f31b86cb104e0227b0f
huongtopher <chrishuong95@gmail.com>
2023-07-17 21:42:14 -0500

    a

--- clpn.R
+++ clpn.R
@@ -5,7 +5,7 @@ library(tidyverse)
 dat <- read.csv('phq9.csv')
 
 
-dat1 <- drop_na(dat)
+dat <- drop_na(dat)
 
 
 glimpse(dat)
@@ -17,6 +17,29 @@ dat[1,3]
 dat$participant_id <- as.factor(dat$participant_id)
 
 summary(dat$participant_id)
+table(dat$participant_id)
+
+
+dat <- dat %>%
+  group_by(participant_id) %>%
+  summarise('phq9 mean' = mean(sum_phq9),
+            'phq9 sd' = sd(sum_phq9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
