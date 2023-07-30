50fdd9fcbcff0fa5a2241b14771ce30cf239c6ec
huongtopher <chrishuong95@gmail.com>
2023-07-30 17:49:24 -0500

    ASD

--- brighten_cleaning.R
+++ brighten_cleaning.R
@@ -6,7 +6,7 @@ phq_long <- read.csv('datasets/phq9_long.csv') #original file name =
 
 phq_base <- read.csv('datasets/phq9_baseline.csv') #original file name =
 
-demo <- read.csv('datasets/demographics_baseline.csv') #original file name =
+
 
 # passive_v1 <- read.csv('datasets/passive_v1.csv')
 
@@ -246,30 +246,151 @@ which(is.na(phq_long3$sum_phq9) != is.na(phq_long3$phq9_9))
 
 
 # Remove participants with more than 3 rows of data with NA in "sum_phq9"
+###
+###   ARBITRARY CHOICE ALERT #####################################
+###
 phq_long3 <- phq_long3 %>%
   group_by(participant_id) %>%
   filter(sum(!is.na(sum_phq9)) > 3) %>%
   ungroup()
 
 
-options(dplyr.width = Inf)
+# options(dplyr.width = Inf)
 
-# participants left in analysis
+# 2028 -> 522 participants left in analysis
 n_distinct(phq_long3$participant_id)
 
+colnames(phq_long3) <-c("participant_id", "week", "phq_sum", "phq_date", 
+                        "phq1", "phq2", "phq3",
+                        "phq4", "phq5", "phq6",
+                        "phq7", "phq8", "phq9")
+
+# save(phq_long3, file='phq_long3.RData')
 
 
 # imputation --------------------------------------------------------------
+library(tidyverse)
+library(mice)
+library(miceadds)
+library(naniar)
+library(knitr)
+
+load('phq_long3.RData')
+# bring in some baseline measures for missingness prediction
+demo <- read.csv('datasets/demographics_baseline.csv') #original file name =
+gad <- read.csv('datasets/gad7.csv') #original file name =
+
+
+demo <- demo %>%
+  select(c(participant_id, gender, education, working, marital_status, race, age, study_arm, study))
+  
+gad <- gad %>%
+  select(c(participant_id, gad7_sum)) # should i use individual gad7 items instead?
+
+# may need to do an analysis to check which predictors correlate with missingness
+
+
+# long to wide form
+phq_long3_w <- phq_long3 %>%
+  select(-c(phq_date, phq_sum)) %>%
+  pivot_wider(names_from = week, values_from = starts_with("phq"),
+              names_sep = "_", names_prefix = "week", values_fill = NA)
 
 
 
+# merge baseline with longitudinal data based on common participant_ids across all datasets
+dat <- merge(merge(demo, gad, by = "participant_id", all = F),
+             phq_long3_w, by = "participant_id", all =F)
+
+# n = 522 -> n = 457
+
+dat$participant_id <- as.factor(dat$participant_id)
+dat$gender <- as.factor(dat$gender)
+dat$education <- as.factor(dat$education)
+dat$working <- as.factor(dat$working)
+dat$marital_status <- as.factor(dat$marital_status)
+dat$race <- as.factor(dat$race)
+dat$study_arm <- as.factor(dat$study_arm)
+dat$study <- as.factor(dat$study)
+
+# save(dat, file = 'phq_wide_457.RData')
+
+# missingness patterns ----------------------------------------------------
+md.pattern(phq_long3_w, plot=F) # no idea what's going on
+
+gg_miss_var(dat, show_pct = T)
+
+# return percent of missingness
+percentmiss <- function(x){
+  sum(is.na(x)) / length(x) * 100} 
+# percent missingness per col
+kable(apply(dat, 2, percentmiss),
+      digits = 3)   
+# no missing baseline variables, which is good. we can use those for our prediction matrix
+
+# number of subjects with missing values by percent
+kable(table(apply(dat, 1, percentmiss)),
+      digits = 3)
+
+# prediction matrix. predict subsequent weeks based on previous weeks
+### we assume data is MAR and use all available baseline variables 
+### that are *plausibly* associated with missingness for imputation
+
+# pred <- quickpred(dat, exclude = c('participant_id'))
+
+# or
+
+# Reorder the data frame using the sorted 'phq' columns
+get_week_number <- function(col_name) {
+  as.numeric(gsub(".*week(\\d+)", "\\1", col_name))
+}
+
+# Get the 'phq' columns
+phq_columns <- colnames(dat)[grepl("phq", colnames(dat))]
+
+# Sort the 'phq' columns by the week number
+sorted_phq_columns <- phq_columns[order(sapply(phq_columns, get_week_number))]
+
+# re order column names by week0,2,4,...
+dat1 <- dat %>%
+  select("participant_id", "gender", "education", "working", "marital_status", 
+         "race", "age", "study_arm", "study", "gad7_sum",
+         all_of(sorted_phq_columns)
+  )
+
+colnames(dat) %in% colnames(dat1)
+dplyr::all_equal(dat,dat1)
 
 
+w0 <- c(0, rep(1, 18), rep(0, 54)) # predict week0 from baseline
+w2 <- c(0, rep(1, 27), rep(0, 45)) # predict week2 from baseline + week0 
+w4 <- c(0, rep(1, 36), rep(0, 36)) # predict week4 from baseline + week0 + week2
+w6 <- c(0, rep(1, 45), rep(0, 27)) # etc etc
+w8 <- c(0, rep(1, 54), rep(0, 18))
+w10 <- c(0, rep(1, 63), rep(0, 9))
+w12 <- c(0, rep(1, 72))
+
+# create prediction matrix
+pred_mat <- rbind(w0,w0,w0,w0,w0,w0,w0,w0,w0,
+                  w2,w2,w2,w2,w2,w2,w2,w2,w2,
+                  w4,w4,w4,w4,w4,w4,w4,w4,w4,
+                  w6,w6,w6,w6,w6,w6,w6,w6,w6,
+                  w8,w8,w8,w8,w8,w8,w8,w8,w8,
+                  w10,w10,w10,w10,w10,w10,w10,w10,w10,
+                  w12,w12,w12,w12,w12,w12,w12,w12,w12)
+
+rownames(pred_mat) <- NULL
+
+# specify the imputed variables
+bls <- list(sorted_phq_columns)
+
+rm(w0,w2,w4,w6,w8,w10,w12, phq_columns, sorted_phq_columns, get_week_number)
+
+dat1_imp <- mice(dat1,
+                 maxit=3, m=3, seed=111)
 
 
-
-
-
+save(dat1_imp, file = 'phq_wide_imp.RData')
 
 
 
